5d760bbdce69ccc2afba32ddf563dfaf0fa870bb
Ninomiya66 <wimps6nanchitte@gmail.com>
2022-06-02 13:00:06 +0000

    top画面レイアウト完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/check'
+  end
+  root 'public/homes#top'
+
   # 顧客用
   # URL /customers/sign_in...
   devise_for :customers, skip: [:passwords], controllers: {
@@ -6,11 +14,26 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
 
+  scope module: :public do
+
+    resources :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'check'
+        patch 'out'
+      end
+    end
+
+  end
+
   # 管理者用
   # URL /admin/sign_in...
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
